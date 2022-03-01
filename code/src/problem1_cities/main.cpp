5ff0c6ec434aed1069c09b68dcb686f66f0db4e2
Georgii Zhulikov <l8doku@gmail.com>
2022-03-01 13:26:13 +0300

    WIP 15 213-2

--- code/src/problem1_cities/main.cpp
+++ code/src/problem1_cities/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 
 //     Main task - to implement the function fillCountries to put countries into a container]
 // (vector<Country> or map<std::string, Country>).
@@ -21,17 +22,77 @@ struct Coordinate
 
 struct City
 {
+    std::string name;
+    Coordinate coordinate;
 };
 
 struct Country
 {
+    std::string name;
 };
 
 
 
 void fillCountries(std::istream& inFile, std::vector<Country>& countries)
 {
-    
+    std::string line;
+    std::getline(inFile, line);
+    while(std::getline(inFile, line))
+    {
+        std::stringstream sstr(line);
+        
+        std::string buffer;
+        
+        City newCity;
+        double coordinateLatitude;
+        
+        // city,lat,lng,country,population
+        
+        // city
+        std::getline(sstr, buffer, ',');
+        
+        newCity.name = buffer;
+
+        // lat
+        std::getline(sstr, buffer, ',');
+        newCity.coordinate.latitude = std::stod(buffer);
+        
+        // lng
+        std::getline(sstr, buffer, ',');
+        newCity.coordinate.longitude = std::stod(buffer);
+        
+        // country
+        std::getline(sstr, buffer, ',');
+        std::string countryName = buffer;
+        
+        // population
+        std::getline(sstr, buffer, ',');
+        
+        bool cityFound = false;
+        for (Country& country: countries)
+        {
+            if (country.name == countryName)
+            {
+                // add the new element to the country.cities
+                cityFound = true;
+                break;
+            }
+        }
+        
+        if (!cityFound)
+        {
+            // add a new country
+            // add a new element to its cities
+            // countries[countries.size() - 1].cities is the container of cities
+        }
+        
+        
+//        if (std::find(countries.begin(), countries.end(), countryName) == countries.end())
+//        {
+            
+//        }
+        
+    }
 }
 
 int main()
@@ -43,5 +104,15 @@ int main()
     {
         std::cout<<"OK\n";
     }
+    
+    std::vector<Country> countries;
+    
+    fillCountries(inputFile, countries);
+    
+    for (const Country& country: countries)
+    {
+        std::cout << country.name << std::endl;
+    }
+    
     return 0;
 }
