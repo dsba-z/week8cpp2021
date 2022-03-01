7cc0a7e7e75705fd25d9bf46841eaf921900b403
Georgii Zhulikov <l8doku@gmail.com>
2022-03-01 15:03:51 +0300

    WIP 15 213-1

--- code/src/problem1_cities/main.cpp
+++ code/src/problem1_cities/main.cpp
@@ -21,17 +21,75 @@ struct Coordinate
 
 struct City
 {
+    std::string name;
+    Coordinate coordinate;
+    
+    bool operator <(const City& rhs) const
+    {
+        return name < rhs.name;
+    }
 };
 
 struct Country
 {
+    std::string name;
 };
 
 
 
 void fillCountries(std::istream& inFile, std::vector<Country>& countries)
 {
+    std::string line;
+    // skip header
+    std::getline(inFile, line);
     
+    while (std::getline(inFile, line))
+    {
+        std::stringstream sstr(line);
+        std::string buffer;
+        
+        // city,lat,lng,country,population
+        
+        City newCity;
+        
+        // city name
+        std::getline(sstr, buffer, ',');
+        newCity.name = buffer;
+        
+        // latitude
+        std::getline(sstr, buffer, ',');
+        newCity.coordinate.latitude = std::stod(buffer);
+
+        // longitude
+        std::getline(sstr, buffer, ',');
+
+        // country name
+        std::getline(sstr, buffer, ',');
+        std::string countryName = buffer;
+        
+        // population
+        std::getline(sstr, buffer, ',');
+        
+        
+        bool cityFound = false;
+        for (const Country& country: countries)
+        {
+            if (country.name == countryName)
+            {
+                cityFound = true;
+                // add city to country
+                break;
+            }
+        }
+                
+        if (!cityFound)
+        {
+            // make a new country
+            // add the city to the new country
+        }
+        
+        
+    }
 }
 
 int main()
@@ -43,5 +101,14 @@ int main()
     {
         std::cout<<"OK\n";
     }
+    
+    std::vector<Country> countries;
+    fillCountries(inputFile, countries);
+    
+    for (const Country& country: countries)
+    {
+        std::cout << country.name << std::endl;
+    }
+    
     return 0;
 }
